6229cb3c3dd9a58243d8fe0aeb6aae380853d654
Daisy <d1wanimo@gmail.com>
2022-01-15 17:01:20 -0800

    Nah man I'm lost

--- app/src/main/java/com/example/pictureapp/FirstFragment.java
+++ app/src/main/java/com/example/pictureapp/FirstFragment.java
@@ -1,15 +1,34 @@
 package com.example.pictureapp;
 
+
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.pictureapp.databinding.FragmentFirstBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+
+import java.util.Collections;
+
 
 public class FirstFragment extends Fragment {
 
@@ -44,5 +63,61 @@ public class FirstFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+//    private void requestSignIn() {
+//        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestEmail()
+//                .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
+//                .build();
+//
+//        GoogleSignInClient client = GoogleSignIn.getClient(this, signInOptions);
+//
+//
+//        startActivityForResult(client.getSignInIntent(), 400);
+//    }
+//    // 4:40 https://www.youtube.com/watch?v=rznYp43KLRc
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        switch (requestCode) {
+//            case 400:
+//                if (resultCode == RESULT_OK) {
+//                    handleSignInIntent(data);
+//                }
+//                break;
+//        }
+//    }
+
+//    private void handleSignInIntent(Intent data) {
+//        GoogleSignIn.getSignedInAccountFromIntent(data)
+//                .addOnSuccessListener(new OnSuccessListener<GoogleSignInAccount>() {
+//                    @Override
+//                    public void onSuccess(GoogleSignInAccount googleSignInAccount) {
+//
+//// something is not lettin gme import this class
+//                        GoogleAccountCredential credential = GoogleAccountCredential.
+//                                usingOAUth2(FirstFragment.this, Collections.singleton(DriveScopes.DRIVE_FILE));
+//                    }
+//
+//                    credential.setSelectedAccount(googleSignInAccount.getAccount());
+//
+//                    Drive googleDriveService = new Drive.Builder(
+//                            AndroidHttp.newCompatibleTransport(),
+//                            new GsonFactory(),
+//                            credential)
+//                            .setApplicationName("My Drive Tutorial")
+//                            .build();
+//                    )
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//
+//                    }
+//                });
+
+
+//    }
 
 }
